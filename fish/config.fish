8f971c6dda02d7943a0cf33219bca945810ed1b7
Madhav Sharma <madhavss@umich.edu>
2025-01-11 13:44:54 +0530

    chore: update

---
+++ fish/config.fish
@@ -0,0 +1,3 @@
+source /opt/homebrew/Cellar/chruby-fish/1.0.0/share/fish/vendor_functions.d/chruby.fish
+source /opt/homebrew/Cellar/chruby-fish/1.0.0/share/fish/vendor_conf.d/chruby_auto.fish
+chruby ruby-3.3.5
